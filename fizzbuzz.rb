dc5f9a3d0d98c94c1a835085ed86091710c7cf8e
Parker Catalano <catalano.work@gmail.com>
2017-09-29 18:31:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0 #if the integar is divisible by three with no remainder
+    print "Fizz"
+  elsif int % 5 == 0 #if integar is divisible by five with no remainder
+    print "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0 #if integar is divisible by 3 and 5 with no remainder
+    print "Fizzbuzz"
+  else
+    print "Nil"
 end
+
