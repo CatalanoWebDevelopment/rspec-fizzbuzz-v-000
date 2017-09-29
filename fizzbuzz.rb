4afcbf882a5b683ccbc9da099d843a6bd7893e2e
Parker Catalano <catalano.work@gmail.com>
2017-09-29 18:32:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     print "Fizz"
-  elsif int % 5 == 0 
+  elsif int % 5 == 0
     print "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0 
+  elsif int % 3 == 0 && int % 5 == 0
     print "Fizzbuzz"
   else
     print "Nil"
+  end
 end
